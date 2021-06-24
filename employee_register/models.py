0d21213ed43d403f5ff3524ea81af1ccd7e5f64f
Anjali Upadhyay <ianjali1598@gmail.com>
2021-06-24 23:17:19 +0530

    changes made and file added

--- employee_register/models.py
+++ employee_register/models.py
@@ -11,7 +11,6 @@ class Employee(models.Model):
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=3)
     mobile= models.CharField(max_length=15)
-    position= models.ForeignKey(Position,on_delete=models.CASCADE)#ON DELETE CASCADE constraint is used in to delete the
+    position= models.ForeignKey(Position,on_delete=models.CASCADE)#ON DELETE CASCADE is used in to delete the
                                                                   #rows from the child table automatically, when the rows 
                                                                   # from the parent table are deleted
-                                                                  
